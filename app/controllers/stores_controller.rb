db314ff104fce8e1405de6873ae1d5a38ddeb537
Gianfranco Ruta <gianjrs.98@mail.com>
2017-05-24 10:32:30 -0400

    Agregandro views y acciones a controller de Store

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,23 +1,33 @@
 class StoresController < ApplicationController
-
+  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+  
   def index
-    @store = Store.all
+    if params.key?(:query)
+      @stores = Store.where('stores.name LIKE ?', "%#{params[:query]}%")
+    else
+      @stores = Store.all
+    end
   end
   
-  def create
-    @store = Store.new(store_params)
+ def create
+    @store = Store.create(store_params)
+
     respond_to do |format|
       if @store.save
-        #format.html { redirect_to @store, notice: 'person was successfully created.' }
+        format.html { redirect_to @store, notice: 'person was successfully created.' }
         format.json { render :show, status: :created, location: @store }
       else
+         format.html { render :new }
         format.json { render json: @store.errors, status: :unprocessable_entity }
       end
+    end
+  end
+    
       
   end
 
   def show
-    
+    @stores
   end
 
   def edit
@@ -40,7 +50,4 @@ class StoresController < ApplicationController
   def store_params
     params.require(:store).permit(:name, :kind)
   end
-  
-end
 
-end
