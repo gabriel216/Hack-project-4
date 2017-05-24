069e3be65e221d76fe838e7711c2c602dcc736bb
Gabriel Gomez <gabrielgomez353@gmail.com>
2017-05-24 13:44:09 -0400

    Arreglo del CRUD para crear tiendas

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,5 @@
 class StoresController < ApplicationController
-  skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
+  # skip_before_action :verify_authenticity_token, only: [:create, :update, :destroy]
   
   def index
     if params.key?(:query)
@@ -8,46 +8,44 @@ class StoresController < ApplicationController
       @stores = Store.all
     end
   end
-  
- def create
-    @store = Store.create(store_params)
-
-    respond_to do |format|
-      if @store.save
-        format.html { redirect_to @store, notice: 'person was successfully created.' }
-        format.json { render :show, status: :created, location: @store }
-      else
-         format.html { render :new }
-        format.json { render json: @store.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-    
-      
-  end
 
   def show
-    @stores
+    @store = Store.first
   end
-
+ 
   def edit
-    
   end
-
+ 
   def new
     @store = Store.new
   end
+  
+def create
+    @store = Store.new(store_params)
 
-  def destroy
-    
+    respond_to do |format|
+      if @store.save
+        format.html { redirect_to @store, notice: 'store was successfully created.' }
+        format.json { render :show, status: :created, location: @store }
+      else
+        format.html { render :new }
+        format.json { render json: @store.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
     
   end
+
+  def destroy
+    
+  end
+  
   private
 
   def store_params
     params.require(:store).permit(:name, :kind)
   end
 
+end
